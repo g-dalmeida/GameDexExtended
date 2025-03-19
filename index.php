b59d479780e3d72ac7283213a2410aa998612d4b
g-dalmeida <giordix23@gmail.com>
2025-03-19 13:13:58 +0100

    form

--- index.php
+++ index.php
@@ -1,13 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+
     <title>GAMEDEXEXTENDED</title>
 </head>
-<body>
-    
 
-<form action="" method="POST"></form>
+<body>
+    <div>
+        <form action="" method="POST">
+            <label for="user">Username:</label><br>
+            <input type="text" id="user" name="user"><br>
+            <label for="pass">Password:</label><br>
+            <input type="password" id="pass" name="pass">
+        </form>
+    </div>
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
